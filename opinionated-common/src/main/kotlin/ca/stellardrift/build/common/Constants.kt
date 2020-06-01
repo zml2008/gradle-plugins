9681350cd276b767126c370fb90c1ed2043f6f90
Zach Levis <zml@aoeu.xyz>
2020-06-01 11:33:13 -0700

    don't need this constant

--- opinionated-common/src/main/kotlin/ca/stellardrift/build/common/Constants.kt
+++ opinionated-common/src/main/kotlin/ca/stellardrift/build/common/Constants.kt
@@ -16,7 +16,6 @@
 
 package ca.stellardrift.build.common
 
-internal const val CATEGORY_GENERATION = "generation"
 internal const val UTF_8 = "UTF-8"
 internal const val VERSION_JUNIT_PROPERTY = "version.junit"
 internal const val VERSION_JUNIT_DEFAULT = "5.6.0"
