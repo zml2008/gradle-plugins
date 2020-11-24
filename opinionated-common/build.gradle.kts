456fed4cfd6d4e4e36e8fe6fbec84b6152da47fa
Zach Levis <zml@aoeu.xyz>
2020-11-23 19:03:07 -0800

    common: Bump to next indra version

--- opinionated-common/build.gradle.kts
+++ opinionated-common/build.gradle.kts
@@ -1,7 +1,7 @@
 import ca.stellardrift.build.self.declarePlugin
 
 dependencies {
-    api("net.kyori:indra-common:1.1.1")
+    api("net.kyori:indra-common:1.2.0")
     api("gradle.plugin.org.cadixdev.gradle:licenser:0.5.0")
     api("org.jlleitschuh.gradle:ktlint-gradle:9.2.1")
 }
