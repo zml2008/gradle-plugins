11b37dce4b517c7433cac21c8b3d56b73293a734
zml <zml@stellardrift.ca>
2021-05-10 21:26:56 -0700

    build: Bump gradle and indra versions

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,11 +4,11 @@ import org.cadixdev.gradle.licenser.LicenseExtension
 plugins {
     kotlin("jvm") version "1.4.10" apply false // we must override what we're providing ourself... whoo circular dependencies
 
-    val indraVersion = "2.0.0"
+    val indraVersion = "2.0.3"
     id("net.kyori.indra") version indraVersion apply false
     id("net.kyori.indra.license-header") version indraVersion apply false
     id("net.kyori.indra.publishing.gradle-plugin") version indraVersion apply false
-    id("com.github.ben-manes.versions") version "0.36.0"
+    id("com.github.ben-manes.versions") version "0.38.0"
 }
 
 group = "ca.stellardrift"
