91675d12e10517671e3f5e8b9c654a9d6b9e5978
zml <zml@stellardrift.ca>
2022-10-08 09:28:35 -0700

    release: Prepare for further development on 6.0.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "ca.stellardrift"
-version = "6.0.0"
+version = "6.0.1-SNAPSHOT"
 description = "A suite of plugins to apply defaults preferred for Stellardrift projects"
 
 subprojects {
