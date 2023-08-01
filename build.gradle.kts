0e8307c264255dd2f8447a8fad3d5c5151b19788
Jason Penilla <11360596+jpenilla@users.noreply.github.com>
2023-08-01 15:33:56 -0700

    6.0.2-SNAPSHOT

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "ca.stellardrift"
-version = "6.0.1"
+version = "6.0.2-SNAPSHOT"
 description = "A suite of plugins to apply defaults preferred for Stellardrift projects"
 
 subprojects {
