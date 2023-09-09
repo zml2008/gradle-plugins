b0d77e97848cb9ab9c223b0c8d26d9ec71b278fc
zml <zml@stellardrift.ca>
2023-09-09 10:48:29 -0700

    release: target 6.1.0 instead

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,7 @@ plugins {
 }
 
 group = "ca.stellardrift"
-version = "6.0.2-SNAPSHOT"
+version = "6.1.0-SNAPSHOT"
 description = "A suite of plugins to apply defaults preferred for Stellardrift projects"
 
 subprojects {
