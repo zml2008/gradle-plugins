cd20015244a86044c1f65d10c5908f5cfc2154cd
zml <zml@stellardrift.ca>
2025-03-09 16:24:27 -0700

    release: version 6.2.0

--- build.gradle.kts
+++ build.gradle.kts
@@ -10,7 +10,7 @@ plugins {
 }
 
 group = "ca.stellardrift"
-version = "6.2.0-SNAPSHOT"
+version = "6.2.0"
 description = "A suite of plugins to apply defaults preferred for Stellardrift projects"
 
 subprojects {
